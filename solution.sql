c0bbf7f4f9bab75fda22fa361ca19b70a5143abc
thanhtrnnn <thanhbx6a@gmail.com>
2025-12-11 11:23:16 +0700

    update problems 147 -> 156

--- solution.sql
+++ solution.sql
@@ -1,6 +1,6 @@
--- ID: 1f95589f-8a16-4c4d-91ff-22176ff7dbaf
--- Code: DEMO-012
--- Title: Tạo bảng quản lý đơn hàng
+-- ID: e727cf96-1132-4454-bd49-473ad963bbf1
+-- Code: SQL151
+-- Title: Loại hàng furniture do những công ty nào cung cấp, địa chỉ của công ty đó
 -- Yêu cầu: Viết câu lệnh SQL bên dưới
 -- ********************************************
 
